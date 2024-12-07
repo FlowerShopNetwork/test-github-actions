9908d21267efdb4578c372144bc8ddc89cff3a08
Kirk Loretz <kirk.loretz@flowershopnetwork.com>
2024-12-06 19:50:26 -0600

    No changed PHP files

--- private/Classes/SevenFour.php
+++ private/Classes/SevenFour.php
@@ -1,20 +1,6 @@
 <?php
 
-// One
 class SevenFour
-{
-    public int $id;
-    public string $name;
-
-    public function foo() {
-      ?>
-      <?
-    }
-}
-
-?>
-<?
-class TestShortTags
 {
     public int $id;
 }
